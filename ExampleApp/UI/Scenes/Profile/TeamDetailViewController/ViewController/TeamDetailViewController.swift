898c6db91ceee910eacc899c3c119a1e0a1d89af
Nicolas <nicolasvalentinima@gmail.com>
2022-09-28 16:05:51 +0200

    can remove or edit favorite team

--- ExampleApp/UI/Scenes/Profile/TeamDetailViewController/ViewController/TeamDetailViewController.swift
+++ ExampleApp/UI/Scenes/Profile/TeamDetailViewController/ViewController/TeamDetailViewController.swift
@@ -9,12 +9,14 @@ import Foundation
 import UIKit
 
 class TeamDetailViewController: BaseViewController {
+    
     // MARK: - Outlets
     
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Properties
     
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,4 +28,12 @@ class TeamDetailViewController: BaseViewController {
         navigationController?.popViewController(animated: true)
     }
     
+    @IBAction func deleteButtonPressed(_ sender: Any) {
+        UserDefaults.standard.removeObject(forKey: "SelectedTeam")
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func editButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "showEditTeam", sender: nil)
+    }
 }
